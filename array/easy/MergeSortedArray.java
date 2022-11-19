26c1db8e40dec51bdeea733ab09cea2c2267aa60
Godugu Shruthi <92107573+godugushruthi@users.noreply.github.com>
2022-11-20 01:06:31 +0530

    Update MergeSortedArray.java

--- array/easy/MergeSortedArray.java
+++ array/easy/MergeSortedArray.java
@@ -1,1 +1,12 @@
+/* problem link : https://leetcode.com/problems/merge-sorted-array/ */
+/* EXPLANATION : 
+We need to merge the two sorted arrays which are given as input,and we need to store this merged array in the first input array.
+INPUT : [1,2,3,0,0,0],[2,5,6] here we need to fill those zeros using the second array 
+OUTPUT : [1,2,2,3,5,6]
+LOGIC :
+take three pointers say p1,p2,p3 which will be pointing to p1 will be pointing to the 3rd element of arr1 and p2 will be pointing to arr2 last element p3 pointing to arr1
+last element respectively.
+now we will compare elements that are pointed by p2 and p1 and the greatest elem is placed at p3 pointer and decrement the p3,p2 pointer. we will repeat this until the 
+p1 and p2 is equal to 0.
+
 
