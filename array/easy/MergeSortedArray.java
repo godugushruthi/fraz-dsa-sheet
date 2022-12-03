2aad37c8e4ff674312a06fa0bd51c8735c3342a1
Godugu Shruthi <92107573+godugushruthi@users.noreply.github.com>
2022-12-03 21:49:29 +0530

    Update MergeSortedArray.java

--- array/easy/MergeSortedArray.java
+++ array/easy/MergeSortedArray.java
@@ -34,10 +34,14 @@ public class MergeSortedArray
     while(i>=0)
     {
       nums1[k]=nums1[i];
+       i--;
+       k--;
     }
     while(j>=0)
     {
-      nums2[k]=nums2[j];
+      nums1[k]=nums2[j];
+      j--;
+      k--;
     }
   }
 }
