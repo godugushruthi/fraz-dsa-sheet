21ddd24cf279bcb597c98c4b339697c31f7faa82
Godugu Shruthi <92107573+godugushruthi@users.noreply.github.com>
2022-12-03 21:32:45 +0530

    Update MergeSortedArray.java

--- array/easy/MergeSortedArray.java
+++ array/easy/MergeSortedArray.java
@@ -6,17 +6,39 @@ OUTPUT : [1,2,2,3,5,6]
 LOGIC :
 take three pointers say p1,p2,p3.p1 will be pointing to the 3rd element of arr1 and p2 will be pointing to arr2 last element p3 pointing to arr1 last element respectively.
 now we will compare elements that are pointed by p2 and p1 and the greatest elem is placed at p3 pointer and decrement the p3,p2 pointer. we will repeat this until the 
-p1 and p2 is equal to 0.*/
+p1 and p2 is greater than equal to 0.one element will be remained it may be either first elem of nums1 or first elem of nums2. */
 //code:
 public class MergeSortedArray
 {
   public static void main(String args[])
   {
-    
-    
-    
+    int nums1[]={1,2,3,0,0,0};
+    int nums2[]={2,5,6};
+    int m=3,n=3;
+    int i=n-1,j=m-1,k=(m+n)-1;
+    while(i>=0&&j>=0)
+    {
+       if(nums1[i]>nums2[j])
+       {
+          nums1[k]=nums1[i];
+          k--;
+          i--;
+       }
+      else
+      {
+        nums1[k]=nums2[j];
+        j--;
+        k--;
+      }
+    }
+    while(i>=0)
+    {
+      nums1[k]=nums1[i];
+    }
+    while(j>=0)
+    {
+      nums2[k]=nums2[j];
+    }
   }
-  
-  
 }
 
